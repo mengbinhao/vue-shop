aa8384431bcac7a64d63ac3bf829c3094705e372
Jack <mengbinhao2018@gmail.com>
2019-06-03 22:04:56 +0800

    encapsulate axios and invoke backend

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,15 @@
-module.exports = {}
+module.exports = {
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				ws: true,
+				changeOrigin: true,
+				// 重写路径: 去掉路径中开头的'/api'
+				pathRewrite: {
+					'^/api': ''
+				}
+			}
+		}
+	}
+}
