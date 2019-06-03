a43fff56fd661c2d9d057551c9b3b8a46ccdb4da
Jack <mengbinhao2018@gmail.com>
2019-06-03 15:14:00 +0800

    finish shoplist and shoplistitem component, and star functional component

---
+++ src/components/ShopList/ShopList.vue
@@ -0,0 +1,86 @@
+<template>
+	<div class="msite_shop_list">
+		<div class="shop_header">
+			<svg class="icon" aria-hidden="true">
+				<use xlink:href="#icon-settings"></use>
+			</svg>
+			<span class="shop_header_title">附近商家</span>
+		</div>
+		<ShopListItem :shops="shops" />
+	</div>
+</template>
+<script>
+import ShopListItem from '@/components/ShopList/ShopListItem'
+
+export default {
+	components: {
+		ShopListItem
+	},
+	beforeCreate() {
+		this.shops = [
+			{
+				id: 1,
+				src: require('./images/shop/1.jpg'),
+				title: '锄禾日当午，汗滴禾下土',
+				rating: 5,
+				sales: 106
+			},
+			{
+				id: 2,
+				src: require('./images/shop/2.jpg'),
+				title: '锄禾日当午，汗滴禾下土',
+				rating: 4.6,
+				sales: 88
+			},
+			{
+				id: 3,
+				src: require('./images/shop/3.jpg'),
+				title: '锄禾日当午，汗滴禾下土',
+				rating: 3.5,
+				sales: 99
+			},
+			{
+				id: 4,
+				src: require('./images/shop/4.jpg'),
+				title: '锄禾日当午，汗滴禾下土',
+				rating: 4.2,
+				sales: 106
+			}
+		]
+	},
+	data() {
+		return {
+			shops: this.shops
+		}
+	}
+}
+</script>
+<style lang="stylus" scoped>
+@import '../../common/stylus/mixins.styl';
+
+.msite_shop_list {
+  top-border-1px(#e4e4e4);
+  margin-top: 10px;
+  background: #fff;
+
+  .shop_header {
+    padding: 10px 10px 0;
+
+    .icon {
+      width: 1.5em;
+      height: 1.5em;
+      vertical-align: -0.15em;
+      fill: #999;
+      overflow: hidden;
+    }
+
+    .shop_header_title {
+      color: #999;
+      font-size: 18px;
+      line-height: 24px;
+      height: 24px;
+      margin-left: 5px;
+    }
+  }
+}
+</style>
---
+++ src/components/ShopList/Star.vue
@@ -0,0 +1,106 @@
+<script>
+const fullStar = {
+	render(h) {
+		return h('span', {
+			class: {
+				'star-item': true,
+				on: true
+			}
+		})
+	}
+}
+
+const halfStar = {
+	render(h) {
+		return h('span', {
+			class: {
+				'star-item': true,
+				half: true
+			}
+		})
+	}
+}
+
+const offStar = {
+	render(h) {
+		return h('span', {
+			class: {
+				'star-item': true,
+				off: true
+			}
+		})
+	}
+}
+
+export default {
+	name: 'Star',
+	functional: true,
+	props: {
+		stars: {
+			type: Number,
+			default: 0,
+			validator(val) {
+				return val >= 0 && val <= 5
+			}
+		}
+	},
+	render(h, context) {
+		const {
+			props: { stars }
+		} = context
+		function getChildStars(stars, h) {
+			if (stars === 5) {
+				return Array.apply(null, {
+					length: 5
+				}).map(function() {
+					return h(fullStar)
+				})
+			} else if (stars >= 4.5 && stars < 5) {
+				return [
+					...Array.apply(null, {
+						length: 4
+					}).map(function() {
+						return h(fullStar)
+					}),
+					h(halfStar)
+				]
+			} else if (stars >= 4 && stars < 4.5) {
+				return [
+					...Array.apply(null, {
+						length: 4
+					}).map(function() {
+						return h(fullStar)
+					}),
+					h(offStar)
+				]
+			} else if (stars >= 3.5 && stars < 4) {
+				return [
+					...Array.apply(null, {
+						length: 3
+					}).map(function() {
+						return h(fullStar)
+					}),
+					h(halfStar),
+					h(offStar)
+				]
+			} else {
+				return Array.apply(null, {
+					length: 5
+				}).map(function() {
+					return h(offStar)
+				})
+			}
+		}
+		return h(
+			'div',
+			{
+				class: {
+					star: true,
+					'star-24': true
+				}
+			},
+			getChildStars(stars, h)
+		)
+	}
+}
+</script>
