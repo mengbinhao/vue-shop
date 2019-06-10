83581f10a880311b0a8edcbda0356eb3449d7b47
Jack <mengbinhao2018@gmail.com>
2019-06-10 16:47:54 +0800

    1 切换登陆样式 2 手机合法检查 3 倒计时效果 4 切换显示隐藏密码 5 前台验证提示 6 提示组件

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
 </script>
 
 <style lang="stylus">
-app {
+#app {
   width: 100%;
   height: 100%;
   background: #f5f5f5;
--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -4,45 +4,99 @@
 			<div class="login_header">
 				<h2 class="login_logo">硅谷外卖</h2>
 				<div class="login_header_title">
-					<a href="javascript:;" class="on">短信登录</a>
-					<a href="javascript:;">密码登录</a>
+					<a
+						href="javascript:;"
+						:class="{ on: loginWay }"
+						@click="loginWay = true"
+						>短信登录</a
+					>
+					<a
+						href="javascript:;"
+						:class="{ on: !loginWay }"
+						@click="loginWay = false"
+						>密码登录</a
+					>
 				</div>
 			</div>
 			<div class="login_content">
-				<form>
-					<div class="on">
+				<form @submit.prevent="login">
+					<div :class="{ on: loginWay }">
 						<section class="login_message">
-							<input type="tel" maxlength="11" placeholder="手机号" />
-							<button disabled="disabled" class="get_verification">
-								获取验证码
+							<input
+								v-focus
+								type="tel"
+								maxlength="11"
+								placeholder="手机号"
+								v-model="phone"
+							/>
+							<button
+								:disabled="!rightPhone"
+								class="get_verification"
+								:class="{ rightPhone }"
+								@click.prevent="getVerificationCode"
+							>
+								{{ countdown > 0 ? `已发送(${countdown}s)` : '获取验证码' }}
 							</button>
 						</section>
 						<section class="login_verification">
-							<input type="tel" maxlength="8" placeholder="验证码" />
+							<input
+								type="tel"
+								maxlength="8"
+								placeholder="验证码"
+								v-model="verificationCode"
+							/>
 						</section>
 						<section class="login_hint">
 							温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
 							<a href="javascript:;">《用户服务协议》</a>
 						</section>
 					</div>
-					<div>
+					<div :class="{ on: !loginWay }">
 						<section>
 							<section class="login_message">
 								<input
 									type="tel"
 									maxlength="11"
 									placeholder="手机/邮箱/用户名"
+									v-model="name"
 								/>
 							</section>
 							<section class="login_verification">
-								<input type="tel" maxlength="8" placeholder="密码" />
-								<div class="switch_button off">
-									<div class="switch_circle"></div>
-									<span class="switch_text">...</span>
+								<input
+									type="text"
+									maxlength="8"
+									v-if="showPassword"
+									v-model="password"
+									placeholder="密码"
+								/>
+								<input
+									type="password"
+									maxlength="8"
+									placeholder="密码"
+									v-model="password"
+									v-else
+								/>
+								<div
+									class="switch_button"
+									:class="showPassword ? 'on' : 'off'"
+									@click="showPassword = !showPassword"
+								>
+									<div
+										class="switch_circle"
+										:class="{ right: showPassword }"
+									></div>
+									<span class="switch_text">
+										{{ showPassword ? 'abc' : '...' }}
+									</span>
 								</div>
 							</section>
 							<section class="login_message">
-								<input type="text" maxlength="11" placeholder="验证码" />
+								<input
+									type="text"
+									maxlength="11"
+									placeholder="验证码"
+									v-model="captcha"
+								/>
 								<img
 									class="get_verification"
 									src="./images/captcha.svg"
@@ -61,12 +115,85 @@
 				</svg>
 			</a>
 		</div>
+		<AlertTip
+			:show-tip-text="showTipText"
+			v-if="isShowTip"
+			@closeTip="closeTip"
+		/>
 	</div>
 </template>
 <script>
+import AlertTip from '@/components/AlertTip'
+
 export default {
 	data() {
-		return {}
+		return {
+			loginWay: true,
+			phone: '',
+			verificationCode: '',
+			name: '',
+			password: '',
+			captcha: '',
+			countdown: 0,
+			showPassword: false,
+			isShowTip: false,
+			showTipText: ''
+		}
+	},
+	components: {
+		AlertTip
+	},
+	methods: {
+		getVerificationCode() {
+			if (!this.countdown) {
+				this.countdown = 10
+				const intervalId = setInterval(() => {
+					this.countdown--
+					if (this.countdown <= 0) {
+						clearInterval(intervalId)
+					}
+				}, 1000)
+
+				//send Ajax
+			}
+		},
+		showTip(message) {
+			this.isShowTip = true
+			this.showTipText = message
+		},
+		login() {
+			if (this.loginWay) {
+				const { rightPhone, verificationCode } = this
+				if (!rightPhone) {
+					this.showTip('need correct phone')
+				} else if (!/^\d{6}$/.test(verificationCode)) {
+					this.showTip('need correct verificationCode')
+				}
+			} else {
+				const { name, password, captcha } = this
+				if (!name) {
+					this.showTip('eed correct name')
+				} else if (!password) {
+					this.showTip('eed correct password')
+				} else if (!captcha) console.log(this.showTip('eed correct captcha'))
+			}
+		},
+		closeTip() {
+			this.isShowTip = false
+			this.showTipText = ''
+		}
+	},
+	computed: {
+		rightPhone() {
+			return /^1\d{10}$/.test(this.phone)
+		}
+	},
+	directives: {
+		focus: {
+			inserted(el) {
+				el.focus()
+			}
+		}
 	}
 }
 </script>
@@ -153,6 +280,10 @@ export default {
               color: #ccc;
               font-size: 14px;
               background: transparent;
+
+              &.rightPhone {
+                color: black;
+              }
             }
           }
 
@@ -203,6 +334,10 @@ export default {
                 background: #fff;
                 box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
                 transition: transform 0.3s;
+
+                &.right {
+                  transform: translateX(30px);
+                }
               }
             }
           }
